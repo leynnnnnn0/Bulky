a9b9b36a67a9420f277ad1bea0c3e11a8dd86cfc
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-06-29 22:12:15 +0800

    1:40

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // How can we say that a property is the primary key
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        [Required]
+        public int DisplayOrder { get; set; }
+
+    }
+}
